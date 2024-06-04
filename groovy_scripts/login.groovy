a6817db1efb9ad5dd7044608986a534b166c754b
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-06-04 13:19:57 -0400

    Update login.groovy

--- groovy_scripts/login.groovy
+++ groovy_scripts/login.groovy
@@ -1,22 +1,9 @@
-/*
- * Create an admin user.
- */
 import jenkins.model.*
 import hudson.security.*
-
-println "--> creating admin user"
-
-def adminUsername = System.getenv("JENKINS_ADMIN_USER")
-def adminPassword = System.getenv("JENKINS_ADMIN_PASSWORD")
-
-assert adminUsername != null : "No JENKINS_ADMIN_USER env var provided, but required"
-assert adminPassword != null : "No JENKINS_ADMIN_PASSWORD env var provided, but required"
-
+ 
+def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount(adminUsername, adminPassword)
-Jenkins.instance.setSecurityRealm(hudsonRealm)
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(false)
-Jenkins.instance.setAuthorizationStrategy(strategy)
-
-Jenkins.instance.save()
+ 
+hudsonRealm.createAccount("default", "default")
+instance.setSecurityRealm(hudsonRealm)
+instance.save()
