6dd6a27f3fb4b4b8128b4002fefcd82d672e65c4
VaishnaviNEU <145122714+VaishnaviNEU@users.noreply.github.com>
2024-06-11 19:11:35 -0400

    Update JenkinsAMI.groovy

--- groovy_scripts/JenkinsAMI.groovy
+++ groovy_scripts/JenkinsAMI.groovy
@@ -4,26 +4,13 @@ multibranchPipelineJob('Jenkins-AMI') {
     branchSources {
         github {
             id('Jenkins-AMI')
-            apiUri('https://api.github.com')
             repoOwner('cyse7125-su24-team16')
             repository('ami-jenkins')
             scanCredentialsId('github_token')
+            buildForkPRMerge(true)
+            buildOriginBranch(false)
+            buildOriginBranchWithPR(false)
             includes('*')
-        }
-    }
-    configure {
-        def traits = it / 'sources' / 'data' / 'jenkins.branch.BranchSource' / 'source' / 'traits'
-        traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
-    }
-    configure { node ->
-        def webhookTrigger = node / triggers / 'com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger' {
-            spec('')
-            token("Jenkins-AMI")
-        }
-    }
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('Jenkinsfile')
         }
     }
 }
