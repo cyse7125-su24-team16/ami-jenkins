3d155972f0f347144177bb340539989aab5581f3
VaishnaviNEU <mantri.v@northeastern.edu>
2024-06-13 10:04:25 -0400

    feat: Add new pipeline stage for deployment

--- Scripts/Confirguring_Jenkins.sh
+++ Scripts/Confirguring_Jenkins.sh
@@ -17,7 +17,7 @@ wget --quiet https://github.com/jenkinsci/plugin-installation-manager-tool/relea
 sudo java -jar ./jenkins-plugin-manager-2.13.0.jar --war /usr/share/java/jenkins.war --plugin-download-directory /var/lib/jenkins/plugins --plugin-file /home/ubuntu/plugins.txt
  
 #Creating a tar file of all the associated configuration files.
-tar -czvf configs.tgz ./jcasc.yaml ./groovy_scripts/helloworld.groovy ./groovy_scripts/wizard.groovy ./groovy_scripts/login.groovy ./groovy_scripts/static-site.groovy ./groovy_scripts/JenkinsAMI.groovy ./groovy_scripts/StaticSiteMultiPipeline.groovy ./groovy_scripts/Terraform.groovy ./groovy_scripts/Helm-Webapp-Cve-Processor ./groovygroovy_scripts/k8s-manifests.groovy 
+tar -czvf configs.tgz ./jcasc.yaml ./groovy_scripts/helloworld.groovy ./groovy_scripts/wizard.groovy ./groovy_scripts/login.groovy ./groovy_scripts/static-site.groovy ./groovy_scripts/JenkinsAMI.groovy ./groovy_scripts/StaticSiteMultiPipeline.groovy ./groovy_scripts/Terraform.groovy ./groovy_scripts/Helm-Webapp-Cve-Processor.groovy ./groovy_scripts/k8s-manifests.groovy 
  
 #Change the ownership of the tar file to jenkins.
 sudo chown ubuntu:ubuntu configs.tgz
@@ -65,14 +65,13 @@ sudo mkdir -p /etc/systemd/system/jenkins.service.d/
 {
   echo "[Service]"
   echo "Environment=\"JAVA_OPTS=-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false -Dcasc.jenkins.config=/var/lib/jenkins/jcasc.yaml\""
-  echo "TimeoutStartSec=600"
 } | sudo tee /etc/systemd/system/jenkins.service.d/override.conf
 
    # Increase Jenkins service timeout and check status and logs
 echo 'Configuring Jenkins service timeout and checking status...'
 sudo systemctl daemon-reload
-sudo systemctl enable jenkins
-sudo systemctl restart jenkins
+sudo systemctl stop jenkins
+sudo systemctl start jenkins
 sudo systemctl status jenkins
 
 # # Set execute permissions for Groovy scripts
