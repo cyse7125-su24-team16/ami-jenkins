ea7cf0da8a02d0592eecfc7f417ba61014a3b73a
VaishnaviNEU <mantri.v@northeastern.edu>
2024-06-13 22:25:28 -0400

    fix(Jenkins): Updated for Push and Pull Steps

--- Scripts/Confirguring_Jenkins.sh
+++ Scripts/Confirguring_Jenkins.sh
@@ -17,7 +17,7 @@ wget --quiet https://github.com/jenkinsci/plugin-installation-manager-tool/relea
 sudo java -jar ./jenkins-plugin-manager-2.13.0.jar --war /usr/share/java/jenkins.war --plugin-download-directory /var/lib/jenkins/plugins --plugin-file /home/ubuntu/plugins.txt
  
 #Creating a tar file of all the associated configuration files.
-tar -czvf configs.tgz ./jcasc.yaml ./groovy_scripts/helloworld.groovy ./groovy_scripts/wizard.groovy ./groovy_scripts/login.groovy ./groovy_scripts/static-site.groovy ./groovy_scripts/JenkinsAMI.groovy ./groovy_scripts/StaticSiteMultiPipeline.groovy ./groovy_scripts/Terraform.groovy ./groovy_scripts/Helm-Webapp-Cve-Processor.groovy ./groovy_scripts/k8s-manifests.groovy 
+tar -czvf configs.tgz ./jcasc.yaml ./groovy_scripts/helloworld.groovy ./groovy_scripts/wizard.groovy ./groovy_scripts/login.groovy ./groovy_scripts/static-site.groovy ./groovy_scripts/JenkinsAMI.groovy ./groovy_scripts/StaticSiteMultiPipeline.groovy ./groovy_scripts/Terraform.groovy ./groovy_scripts/Helm-Webapp-Cve-Processor.groovy ./groovy_scripts/k8s-manifests.groovy ./groovy_scripts/Webapp-Cve-Processor.groovy
  
 #Change the ownership of the tar file to jenkins.
 sudo chown ubuntu:ubuntu configs.tgz
