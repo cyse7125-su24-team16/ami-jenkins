c526e25e4e158e7c5f3fbf9338bb491bc0143d55
Anu-3980 <senthilnathan.a@northeastern.edu>
2024-07-16 21:57:03 -0400

    Added the autoscaler groovy

---
+++ groovy_scripts/eks-autoscaler.groovy
@@ -0,0 +1,16 @@
+multibranchPipelineJob('EKS-Autoscaler-Job') {
+    displayName('EKS-Autoscaler-Job')
+    description('Validate Push and Pull Conventional Messages.')
+    branchSources {
+        github {
+            id('EKS-Autoscaler')
+            repoOwner('cyse7125-su24-team16')
+            repository('helm-eks-autoscaler')
+            scanCredentialsId('github_token')
+            buildForkPRMerge(true)
+            buildOriginBranch(true)
+            buildOriginBranchWithPR(false)
+            includes('*')
+        }
+    }
+}
